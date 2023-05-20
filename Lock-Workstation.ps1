cce9a8bf0957da0cf679577a0009fc71f127cc86
Elad Levi <58475124+JakePeralta7@users.noreply.github.com>
2023-05-20 23:31:23 +0300

    Create Lock-Workstation.ps1

---
+++ Lock-Workstation.ps1
@@ -0,0 +1,6 @@
+﻿$code = @'
+[DllImport("user32.dll")]
+public static extern void LockWorkStation();
+'@
+$winApi = Add-Type -MemberDefinition $code -Name WinAPI -Namespace Extern -PassThru
+$winApi::LockWorkStation()
